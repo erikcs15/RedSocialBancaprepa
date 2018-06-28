5e7f8537c0530cbda9677b61e06b95cd56b9954e
axenco <AXEL_ENRIQUE_cORU@HOTMAIL.COM>
2018-06-28 15:57:57 -0600

    jquery
    agregamos php y validamos login

--- js/bancaprepa.js
+++ js/bancaprepa.js
@@ -1,5 +1,23 @@
 $(document).ready(function(){
 
+    $( "#btn_login" ).click(function() { 
+        var correo='';
+        var pass='';
+        correo = $("#email").val();
+        pass =   $("#password").val()
 
-    
+        if(correo==''){
+            M.toast({html: 'El correo no puede ir vacio', classes: 'rounded red'}); 
+            return;
+        }
+
+        if(pass==''){
+            M.toast({html: 'La contraseña no puede ir vacia', classes: 'rounded red'}); 
+            return;
+        }   
+
+
+            
+     });
+
 });
