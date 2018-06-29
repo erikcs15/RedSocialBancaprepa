e8068ea0c3369482cb9b6809fa827e614ab9ed54
axenco <AXEL_ENRIQUE_cORU@HOTMAIL.COM>
2018-06-29 11:57:11 -0600

    bancaprepa
    ok

--- js/bancaprepa.js
+++ js/bancaprepa.js
@@ -1,11 +1,16 @@
+// Funcion principal de Jquery la cual escanea en tiempo real nuestro documento para verificar que los eventos se ejecuten correctamente
 $(document).ready(function(){
 
+    //Inicia el evento click en el boton del login
     $( "#btn_login" ).click(function() { 
+        //inicializamos variables
         var user='';
         var pass='';
+        //igualamos variables con jquery obtenemos el valor '.val()' del id indicado con '$(#ID)'
         user = $("#user").val();
-        pass =   $("#password").val()
+        pass =  $("#password").val();
 
+        // Validamos que los campos no esten vacios
         if(user==''){
             M.toast({html: 'El nombre de usuario no puede ir vacio', classes: 'rounded red'}); 
             return;
@@ -15,14 +20,27 @@ $(document).ready(function(){
             M.toast({html: 'La contraseña no puede ir vacia', classes: 'rounded red'}); 
             return;
         }   
-
+       // llamamos a funcion de ajax para conectar con nuestro servidor php funcion(parametro1,parametro2), el primer parametro es un JSON y el segun una funcion de respuesta en donde recibiremos los datos que nos regrese  ajax
         onRequest({ opcion : 1 ,usuario:user,password:pass },respUser);
             
      });
+     // Fin de click en login
+
+     $("#user").keypress(function(e) {
+        if(e.which == 13) {
+            $( "#password" ).focus();
+        }
+    });
+
+    $("#password").keypress(function(e) {
+        if(e.which == 13) {
+            $( "#btn_login" ).click();
+        }
+    });
 
 });
 
-
+// Funcion de respuesta de la consulta que aplicamos con ajax
 var respUser = function(data) { 
 
     if (!data && data == null) 
