5034535d08951efbdb99ef5c7bf918f1793284fd
axenco <AXEL_ENRIQUE_cORU@HOTMAIL.COM>
2018-07-20 17:13:22 -0600

    ok
    ok

--- menu/menu.php
+++ menu/menu.php
@@ -7,55 +7,25 @@
                         <ul class="right hide-on-med-and-down">
                             <li> <a class='dropdown-button waves-effect waves-dark ' href='#' data-activates='dropdown_message'><i class="material-icons">notifications_active</i><span class="counts black-text">9+</span></a>
                             <ul id='dropdown_message' class='dropdown-content messages collection'>
-                                <!--<li class="collection-item"> <img src="/RedSocialBancaprepa/img/pic2.png" alt="" class="circle"> <span class="title">Max Smith</span>
-                                    <p>Last Updated: 2:00 AM<br>
-                                    <b>Author: maxartkiller.in</b> </p>
-                                </li>
-                                <li class="collection-item avatar"> <i class="material-icons circle">folder</i> <span class="title">Photos</span>
-                                <p>Last Updated: 2:00 AM<br>
-                                    <b>Author: maxartkiller.in</b> </p>
-                                </li>
-                                <li class="collection-item avatar"> <i class="material-icons circle green">insert_chart</i> <span class="title">Analytics</span>
-                                <p>Last Updated: 2:00 AM<br>
-                                    <b>Author: maxartkiller.in</b> </p>
-                                </li>
-                                <li class="collection-item avatar"> <i class="material-icons circle red">play_arrow</i> <span class="title">Play it now!</span>
-                                <p>Last Updated: 2:00 AM<br>
-                                    <b>Author: maxartkiller.in</b> </p>
-                                </li>
-                                <li class="collection-item center-align"> View all </li>-->
+                             
                             </ul>
                             </li>
                             <li> <a class='dropdown-button waves-effect waves-dark' href='#' data-activates='dropdown_task'><i class="material-icons">message</i><span class="counts black-text">9+</span></a>
-                            <ul id='dropdown_task' class='dropdown-content listitems collection'>
-                                <li class="collection-item row">
-                                <div class="col s2"> <img src="/RedSocialBancaprepa/img/pic2.png" alt="" class="circle responsive-img"> <!-- notice the "circle" class --> 
-                                </div>
-                                <div class="col s10"> <span class="black-text"> This is best ever template we have seen and it will remain our first choice to select.<br>
-                                    <small>12:00 am</small> </span> </div>
-                                </li>
-                                <li class="collection-item row">
-                                <div class="col s2"> <img src="../img/pic2.png" alt="" class="circle responsive-img"> <!-- notice the "circle" class --> 
-                                </div>
-                                <div class="col s10"> <span class="black-text">This is best ever template we have seen and it will remain our first choice to select.<br>
-                                    <small>12:00 am</small> </span> </div>
-                                </li>
-                                <li class="collection-item row">
-                                <div class="col s2"> <img src="../img/pic2.png" alt="" class="circle responsive-img"> <!-- notice the "circle" class --> 
-                                </div>
-                                <div class="col s10"> <span class="black-text">This is best ever template we have seen and it will remain our first choice to select.<br>
-                                    <small>12:00 am</small> </span> </div>
-                                </li>
-                                <li class="collection-item center-align"> View all </li>
-                            </ul>
+                            
                             </li>
                             <ul id="dropdownCuenta" class="dropdown-content">
                                 <li class="divider"></li>
-                                <li><a href="#!"><i class="material-icons right">account_circle</i>Cuenta</a></li>
+                                <li><a href="#!"><i class="material-icons right">account_circle</i><?php
+                                echo $_COOKIE["b_capturista"];
+                                ?></a></li>
                                 <li><a id="salirsesion" name ="salirsesion" class="waves-effect">Cerrar sesión<i class="material-icons">settings_power</i></a></li>
                                
                             </ul>
-                            <li><a class="dropdown-trigger" href="#!" data-target="dropdownCuenta"><i class="material-icons right">account_circle</i></a></li>
+                            <li><a class="dropdown-trigger" href="#!" data-target="dropdownCuenta"><i class="material-icons left">account_circle</i>
+                            <?php
+                                echo $_COOKIE["b_capturista"];
+                                ?>
+                            </a></li>
                             
                     </div>
             </nav>
@@ -66,12 +36,7 @@
                 <div class="background">
                   <img src="/RedSocialBancaprepa/img/logo_bancaprepa.png"  style="width: 290px;">
                 </div>
-                <br>
-                <a><span class="black-text name" id="nombreusuario"><i class="small material-icons">account_circle</i><strong>
-                  <?php
-                   echo $_COOKIE["b_capturista"];
-                  ?>
-                </strong></span></a>
+                <br> 
               </div></li>
               <li><a id="inicio" name ="inicio" class="waves-effect"><i class="material-icons">home</i>Inicio</a></li>
               <li><a class="waves-effect modal-trigger" href="#modalAgregarPub"><i class="material-icons">add_circle</i>Agregar publicación</a></li>
